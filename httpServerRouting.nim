97e8996821540fb61579a7357c1778f0f0bdcb71
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-08-07 16:38:01 +0100

    Improved Stuffs

--- httpServerRouting.nim
+++ httpServerRouting.nim
@@ -1,12 +1,16 @@
 # Basic HTTP Server, as via http://nimrod-lang.org/httpserver.html
-
 import strutils, sockets, httpserver
 
-var counter = 0
 proc handleRequest(client, path, query): bool {.procvar.} =
-  inc(counter)
-  client.send("Hello for the $#th time. Your Path is:" % $counter & path & wwwNL)
+  client.send("<!-- Hello. Your Path is: " & path & "-->" & wwwNL)
   echo("Request Made: " & path & "")
+  writeFile("data","")
+  case path
+  of "/":
+    client.send("Home" & wwwNL)
+  else:
+    client.send("404" & wwwNL)
   return false # do not stop processing
 
 run(handleRequest, TPort(8080))
+
